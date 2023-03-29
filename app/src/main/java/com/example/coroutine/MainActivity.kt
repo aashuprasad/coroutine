6498cdd603aa7738dcc6725e8c8dfcdb1187cfdb
Ashu Prasad <aashuprasad@live.com>
2023-03-29 18:19:32 +0530

    threads

--- app/src/main/java/com/example/coroutine/MainActivity.kt
+++ app/src/main/java/com/example/coroutine/MainActivity.kt
@@ -2,17 +2,42 @@ package com.example.coroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.*
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import kotlinx.coroutines.DefaultExecutor.thread
+import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
-    private val TAG:String = "KOTLIN_DBG"
+    private val TAG:String = "KOTLIN_DEBUG"
+    lateinit var counterText : TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
+        counterText = findViewById(R.id.counter)
+        Log.d(TAG, "${Thread.currentThread().name}")
     }
+
+    fun updateCounter(view: View) {
+        counterText.text = "${counterText.text.toString().toInt() + 1}"
+        Log.d(TAG, "${Thread.currentThread().name}")
+    }
+
+    fun executeLongRunningTask() {
+        for (i in 1..1000000000L){
+
+        }
+    }
+
+    fun doAction(view: View) {
+        thread(start = true) {
+            executeLongRunningTask()
+        }
+    }
+
+
 }
