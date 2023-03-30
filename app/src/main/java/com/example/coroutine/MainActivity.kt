ba12342da25d6e0e93ce1fd0006fb62b4f5a449b
Ashu Prasad <aashuprasad@live.com>
2023-03-30 11:57:04 +0530

    launch vs async

--- app/src/main/java/com/example/coroutine/MainActivity.kt
+++ app/src/main/java/com/example/coroutine/MainActivity.kt
@@ -1,43 +1,43 @@
 package com.example.coroutine
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.TextView
-import kotlinx.coroutines.DefaultExecutor.thread
-import kotlin.concurrent.thread
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
-    private val TAG:String = "KOTLIN_DEBUG"
-    lateinit var counterText : TextView
+    private val TAG: String = "KOTLIN_DEBUG"
+    lateinit var counterText: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        counterText = findViewById(R.id.counter)
-        Log.d(TAG, "${Thread.currentThread().name}")
-    }
+        CoroutineScope(Dispatchers.Main).launch {
+            task1()
+        }
 
-    fun updateCounter(view: View) {
-        counterText.text = "${counterText.text.toString().toInt() + 1}"
-        Log.d(TAG, "${Thread.currentThread().name}")
-    }
-
-    fun executeLongRunningTask() {
-        for (i in 1..1000000000L){
-
+        CoroutineScope(Dispatchers.Main).launch {
+            task2()
         }
     }
 
-    fun doAction(view: View) {
-        thread(start = true) {
-            executeLongRunningTask()
-        }
+    suspend fun task1() {
+        Log.d(TAG, "STARTING TASK 1")
+        delay(3000)
+        Log.d(TAG, "ENDING TASK 1")
     }
 
+    suspend fun task2() {
+        Log.d(TAG, "STARTING TASK 2")
+        delay(2500)
+        Log.d(TAG, "ENDING TASK 2")
+    }
 
 }
